789c3a6ec7d4c47771f970bdade48d0c6f22c9f8
Arilson Souza <arilsons61@gmail.com>
2023-03-09 19:56:30 -0300

    feat: finish QuantityInput component

--- src/components/QuantityInput/index.tsx
+++ src/components/QuantityInput/index.tsx
@@ -1,12 +1,29 @@
 import { Minus, Plus } from 'phosphor-react'
 import { QuantityInputContainer } from './styles'
 
-export function QuantityInput() {
+interface QuantityInputProps {
+  quantity: number
+  onQuantityChange: (quantity: number) => void
+}
+
+export function QuantityInput({
+  quantity,
+  onQuantityChange,
+}: QuantityInputProps) {
+  function handleIncreaseQauntity() {
+    onQuantityChange(quantity + 1)
+  }
+
+  function handleDecreaseQauntity() {
+    if (quantity - 1 >= 1) {
+      onQuantityChange(quantity - 1)
+    }
+  }
   return (
     <QuantityInputContainer>
-      <Minus size={22} />
-      <span>1</span>
-      <Plus size={22} />
+      <Minus size={22} onClick={handleDecreaseQauntity} />
+      <span>{quantity}</span>
+      <Plus size={22} onClick={handleIncreaseQauntity} />
     </QuantityInputContainer>
   )
 }
